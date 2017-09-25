af3d6811b4de5327b2fd193714959bfac727ae7e
Craig Knott <cknott@atlassian.com>
2017-09-25 00:33:53 -0500

    Updated index and login pages

--- spotify_flask_web_api/views.py
+++ spotify_flask_web_api/views.py
@@ -2,7 +2,7 @@ import requests
 import urllib
 
 from uuid import uuid4
-from flask import render_template, request, redirect, jsonify, session
+from flask import render_template, request, redirect, session
 
 from spotify_flask_web_api import app
 
@@ -16,10 +16,15 @@ SCOPE = 'user-read-private user-read-email'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'access_token' in session:
+        r = requests.get('https://api.spotify.com/v1/me',
+                         headers={'Authorization': 'Bearer ' + session.get('access_token')})
+        r.raise_for_status()
+        return render_template('index.html', data=r.json())
+    return render_template('login.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET'])
 def login():
     state = str(uuid4())
     session[STATE_KEY] = state
@@ -53,8 +58,20 @@ def callback():
     r.raise_for_status()
     session['access_token'] = r.json().get('access_token')
     session['refresh_token'] = r.json().get('refresh_token')
+    return redirect('/')
 
-    # use the access token to access the Spotify Web API
-    r = requests.get('https://api.spotify.com/v1/me', headers={'Authorization': 'Bearer ' + session['access_token']})
+
+@app.route('/refresh_token', methods=['GET'])
+def refresh_token():
+    r = requests.post('https://accounts.spotify.com/api/token',
+                      params={
+                          'grant_type': 'refresh_token',
+                          'refresh_token': session['refresh_token'],
+                          'client_id': CLIENT_ID,
+                          'client_secret': CLIENT_SECRET
+                      },
+                      headers={'Content-Type': 'application/x-www-form-urlencoded'})
     r.raise_for_status()
-    return jsonify(r.json())
+    session['access_token'] = r.json().get('access_token', session['access_token'])
+    session['refresh_token'] = r.json().get('refresh_token', session['refresh_token'])
+    return redirect('/')
